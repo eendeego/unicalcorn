0eda8d7e42beec266f02490a030ba72ffd54d198
Luis Reis <luis.m.reis@gmail.com>
2020-05-01 22:08:36 -0700

    Fetch current meetings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import ical from 'ical';
 import fetch from 'node-fetch';
 
 async function fetchIcalData(url) {
@@ -5,6 +6,38 @@ async function fetchIcalData(url) {
   return (await fetch(url)).text();
 }
 
-const icalData = await fetchIcalData(process.argv[2]);
+function midnightTime(referenceDate) {
+  const date = new Date(referenceDate);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date.getTime();
+}
 
-console.log('ical data size: ' + icalData.length);
+function pad(num) {
+  return num < 10 ? '0' + num.toString() : num.toString();
+}
+
+function dumpEvents(events) {
+  for (const ev of events) {
+    console.log(
+      `${ev.start.getFullYear()}-${pad(ev.start.getMonth() + 1)}-${pad(
+        ev.start.getDate(),
+      )} ${pad(ev.start.getHours())}:${pad(ev.start.getMinutes())} ${
+        ev.summary
+      }`,
+    );
+  }
+}
+
+async function fetchCurrentEvents() {
+  const todayInMs = midnightTime(new Date());
+
+  const icalData = await fetchIcalData(process.argv[2]);
+  return Object.values(ical.parseICS(icalData)).filter(
+    e => e.type === 'VEVENT' && e.start.getTime() >= todayInMs,
+  );
+}
+
+dumpEvents(await fetchCurrentEvents());
