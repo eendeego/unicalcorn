1a1024cc7975b4f706d0800cbb9260a41be773cb
Luis Reis <luis.m.reis@gmail.com>
2020-05-02 00:21:00 -0700

    useEffect implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import {fetchCurrentEvents, dumpEvents} from './calendar-data.js';
-import uiEventLoop from './ui.js';
+// import {fetchCurrentEvents, dumpEvents} from './calendar-data.js';
+import uiEventLoop, {useEffect} from './ui.js';
 
 // eslint-disable-next-line
 function renderCalendar({url}) {
+  useEffect(() => {
+    let handle;
+
+    function updateData() {
+      // fetchCurrentEvents().then(events => dumpEvents(events));
+      console.log('Update');
+      handle = setTimeout(updateData, 3000);
+    }
+    updateData();
+
+    return () => clearTimeout(handle);
+  }, []);
+
   return [];
 }
 
 uiEventLoop(renderCalendar, {url: process.argv[2]});
-
-fetchCurrentEvents().then(events => dumpEvents(events));
--- src/ui.js
+++ src/ui.js
@@ -3,14 +3,37 @@
  */
 
 import deepEqual from 'deep-equal';
+import ShallowEquals from 'shallow-equal/dist/index.cjs.js';
+
+const {shallowEqualArrays} = ShallowEquals;
 
 function paint(sceneGraph) {
   console.log('New Scene Graph: ', sceneGraph);
 }
 
+let hookIndex = 0;
+let hookList = [];
+
+export function useEffect(callback, dependencies) {
+  if (hookIndex === hookList.length) {
+    hookList.push({
+      type: 'effect',
+      callback,
+      dependencies,
+      newDependencies: null,
+      cleanup: () => {},
+    });
+  } else {
+    hookList[hookIndex].newDependencies = dependencies;
+  }
+
+  hookIndex++;
+}
+
 let sceneGraph = [];
 
 export default function uiEventLoop(topComponent, props) {
+  hookIndex = 0;
   const newSceneGraph = topComponent(props);
 
   if (!deepEqual(sceneGraph, newSceneGraph)) {
@@ -18,6 +41,13 @@ export default function uiEventLoop(topComponent, props) {
     paint(sceneGraph);
   }
 
+  for (const hook of hookList) {
+    if (hook.type === 'effect') {
+      if (!shallowEqualArrays(hook.newDependencies, hook.dependencies)) {
+        hook.cleanup = hook.callback();
+      }
+    }
+  }
   // Eventually, this will go away and the update cycle will be triggered by
   // something else
   setTimeout(_ => uiEventLoop(topComponent, props), 167);
