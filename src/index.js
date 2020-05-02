31f6c48399eb740d40e5e9b3eec894fb7a2df4b2
Luis Reis <luis.m.reis@gmail.com>
2020-05-01 20:16:23 -0700

    Fetch a iCal data via URL
    Don't do anything with it yet.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import fetch from 'node-fetch';
+
+async function fetchIcalData(url) {
+  console.log('ical url: ' + url);
+  return (await fetch(url)).text();
+}
+
+const icalData = await fetchIcalData(process.argv[2]);
+
+console.log('ical data size: ' + icalData.length);
