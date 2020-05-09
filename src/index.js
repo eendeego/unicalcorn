aa4391e8b154e8f9ad4c2a23191332e93bfb09ee
Luis Reis <luis.m.reis@gmail.com>
2020-05-09 12:57:43 -0700

    Render events starting before first display row

--- src/index.js
+++ src/index.js
@@ -4,24 +4,20 @@ const {uiEventLoop, useEffect, useState} = require('./ui');
 // const {paint} = require('./console');
 const {paint} = require('./unicorn');
 
-const THREE_DAYS = 3 * 24 * 60 * 60 * 1000;
+const ONE_DAY = 24 * 60 * 60 * 1000;
+const THREE_DAYS = 3 * ONE_DAY;
 
 // eslint-disable-next-line
 function renderCalendar({url}) {
   const [startTime, setStartTime] = useState(() => roundDown(Date.now()));
-  // const [startTime, setStartTime] = useState(() =>
-  //   roundDown(new Date('2020-05-04T09:00:00-0700')),
-  // );
   const [layout, setLayout] = useState(null);
 
   useEffect(() => {
     let handle;
 
     function updateData() {
-      // const now = new Date('2020-05-04T09:00:00-0700').getTime();
-      const now = new Date();
-      fetchEvents(now, now + THREE_DAYS).then(events => {
-        // dumpEvents(events);
+      const now = new Date(startTime).getTime();
+      fetchEvents(now - ONE_DAY, now + THREE_DAYS).then(events => {
         const layout = computeLayout(events);
         setLayout(layout);
       });
@@ -36,14 +32,13 @@ function renderCalendar({url}) {
 
   const result = [];
 
-  // let rowIndex = (layout.start - startTime) / QUARTER_HOUR;
   let firstTimelineIndex = (startTime - layout.start) / QUARTER_HOUR;
   for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
-    if (firstTimelineIndex + rowIndex < 0) {
+    const row = layout.timeline[firstTimelineIndex + rowIndex];
+
+    if (row === undefined) {
       continue;
     }
-
-    const row = layout.timeline[firstTimelineIndex + rowIndex];
 
     row.columns.forEach((event, columnIndex) => {
       if (event.event.start.getTime() === row.time) {
@@ -58,6 +53,27 @@ function renderCalendar({url}) {
               roundDown(event.event.start.getTime())) /
             QUARTER_HOUR,
           color: rowIndex === 0 ? 'red' : rowIndex < 2 ? 'orange' : 'cyan',
+        });
+      } else if (
+        event.event.start.getTime() < startTime &&
+        row.time + QUARTER_HOUR === roundUp(event.event.end.getTime())
+      ) {
+        result.push({
+          type: 'event',
+          event: event.event,
+          x: (columnIndex * 12) / event.rowGroup.width,
+          y:
+            rowIndex -
+            (roundUp(event.event.end.getTime()) -
+              roundDown(event.event.start.getTime())) /
+              QUARTER_HOUR +
+            1,
+          width: 12 / event.rowGroup.width,
+          height:
+            (roundUp(event.event.end.getTime()) -
+              roundDown(event.event.start.getTime())) /
+            QUARTER_HOUR,
+          color: 'red',
         });
       }
     });
