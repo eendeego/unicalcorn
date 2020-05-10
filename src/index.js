ec101d715c49f109c32ee4985aec36ea57ab6e9a
Luis Reis <luis.m.reis@gmail.com>
2020-05-09 22:07:12 -0700

    Add Griffin Powermate support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import PowerMate from 'powermate';
+
 import {fetchEvents, dumpEvent, dumpEvents} from './calendar-data.js';
 import {
   QUARTER_HOUR,
@@ -18,6 +20,7 @@ const CALENDER_UPDATE_INTERVAL = 30000;
 // eslint-disable-next-line
 function renderCalendar({url}) {
   const [startTime, setStartTime] = useState(() => roundDown(Date.now()));
+  const [timeOffset, setTimeOffset] = useState(0);
   const [layout, setLayout] = useState(null);
 
   useEffect(() => {
@@ -50,11 +53,49 @@ function renderCalendar({url}) {
     return () => clearTimeout(handle);
   }, []);
 
+  useEffect(() => {
+    let handle;
+    let powermate;
+
+    function pollForPowerMate() {
+      try {
+        powermate = new PowerMate();
+
+        powermate.on('left', () =>
+          setTimeOffset(timeOffset =>
+            Math.max(timeOffset - QUARTER_HOUR, -ONE_DAY),
+          ),
+        );
+        powermate.on('right', () => {
+          setTimeOffset(timeOffset =>
+            Math.min(timeOffset + QUARTER_HOUR, THREE_DAYS - 16 * QUARTER_HOUR),
+          );
+        });
+        powermate.on('press', () => setTimeOffset(0));
+
+        powermate.on('error', _error => {
+          powermate
+            .eventNames()
+            .forEach(event => powermate.removeAllListeners(event));
+          powermate = null;
+          pollForPowerMate();
+        });
+      } catch (_e) {
+        handle = setTimeout(pollForPowerMate, 1000);
+      }
+    }
+
+    pollForPowerMate();
+
+    return () => clearTimeout(handle);
+  }, []);
+
   if (layout === null) return [];
 
   const result = [];
 
-  let firstTimelineIndex = (startTime - layout.start) / QUARTER_HOUR;
+  let firstTimelineIndex =
+    (startTime + timeOffset - layout.start) / QUARTER_HOUR;
   for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
     const row = layout.timeline[firstTimelineIndex + rowIndex];
 
